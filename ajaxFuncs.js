08c08ed69ccdec016a28e4561e9ccfe18a9d46ee
andrew gibbs <andrewgibbs@outlook.com>
2016-05-23 16:36:27 -0700

    added axaxFunc to call for pill

---
+++ ajaxFuncs.js
@@ -0,0 +1,15 @@
+var rp = require('request-promise')
+
+rp({
+  method: "GET",
+  uri: "http://pillbox.nlm.nih.gov/PHP/pillboxAPIService.php",
+  qs: {
+    key: "F2IWKICTOM",
+        imprint: "p24hr"
+      }
+    })
+    // .then(response => console.log(response))
+    .then(function(response) {
+      console.log(response + " 1");
+    })
+    .catch(err => console.log(err));
