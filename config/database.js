c4b9273573a5a4ee25513a6e40d5c722d97f21c7
andrew gibbs <andrewgibbs@outlook.com>
2016-05-25 14:57:28 -0700

    starting to add deploy

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 
 // Use different database URIs based on whether an env var exists.
 //Will change when modifying .env file
-// var dbUri = process.env.MONGOLAB_URI ||
-//             'mongodb://localhost/' + process.env.LOCAL_DB;
-var dbUri = 'mongodb://localhost/practiceHealthApp'
+var dbUri = process.env.MONGOLAB_URI ||
+            'mongodb://localhost/' + process.env.LOCAL_DB;
+// var dbUri = 'mongodb://localhost/practiceHealthApp'
 if (!process.env.MONGOLAB_URI) {
   // check that MongoD is running...
   require('net').connect(27017, 'localhost').on('error', function() {
